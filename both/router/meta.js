77d9bb34e512bcc573398ff5ccef100b11cc6a44
Maxence Cornet <maxence.cornet20@gmail.com>
2015-10-20 21:46:40 +0200

    Deleted unused packages

--- both/router/meta.js
+++
@@ -1,9 +0,0 @@
-if(Meteor.isClient) {
-  Meta.config({
-      options: {
-        // Meteor.settings[Meteor.settings.environment].public.meta.title
-        title: 'Default Project Title',
-        suffix: 'Project Title'
-      }
-  });
-}
