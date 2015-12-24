7540b93b99e857689002db3dbba8d394251d6fb7
Maxence Cornet <maxence.cornet20@gmail.com>
2015-12-24 17:49:32 +0100

    Created a package to generate form-data/multipart uploadable bundle

---
+++ packages/form-data/package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  name: 'maxencec:form-data',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Generate multipart/form-data from array buffer (Uint8Array), used to upload files to external servers using APIs',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/maxenceC/meteor-form-data',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use(['ecmascript', 'underscore']);
+  api.addFiles('form-data.js');
+  api.export('FormData', 'server')
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('maxencec:form-data');
+  api.addFiles('form-data-tests.js');
+});
